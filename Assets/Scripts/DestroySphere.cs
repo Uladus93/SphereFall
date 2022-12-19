11884e5c0a4ea48a2bb9224ba69d23a9bf14ccf3
Uladus93 <Loscros88@gmail.com>
2022-12-20 01:37:59 +0300

    Added controls
    Added controls for destroying spheres.

--- Assets/Scripts/DestroySphere.cs
+++ Assets/Scripts/DestroySphere.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class DestroySphere : MonoBehaviour
 {
+    private string _tag;
+    private KeyCode _key;
+    private void Start()
+    {
+        _tag = gameObject.tag;
+        switch (_tag)
+        {
+            case "violet": 
+                _key = (KeyCode)(Rainbow.violet + 256);
+                break;
+            case "indigo":
+                _key = (KeyCode)(Rainbow.indigo + 256);
+                break;
+            case "blue":
+                _key = (KeyCode)(Rainbow.blue + 256);
+                break;
+            case "green":
+                _key = (KeyCode)(Rainbow.green + 256);
+                break;
+            case "yellow":
+                _key = (KeyCode)(Rainbow.yellow + 256);
+                break;
+            case "orange":
+                _key = (KeyCode)(Rainbow.orange + 256);
+                break;
+            case "red":
+                _key = (KeyCode)(Rainbow.red + 256);
+                break;
+            default:
+                break;
+        }
+    }
     void FixedUpdate()
     {
         if (transform.position.y <= -5.5)
         {
+            Destroy(gameObject);
+        }
+        else if (Input.GetKey(_key))
+        {
+            Debug.Log(_key + _tag);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Rainbow.cs
+++ Assets/Scripts/Rainbow.cs
@@ -1,10 +1,10 @@
 enum Rainbow
 {
-    violet = 0,
-    indigo = 1,
-    blue = 2,
-    green = 3,
-    yellow = 4,
-    orange = 5,
-    red = 6
+    violet = 1,
+    indigo = 2,
+    blue = 3,
+    green = 4,
+    yellow = 5,
+    orange = 6,
+    red = 7
 }
