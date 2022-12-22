753a8afc1b1006e8dc437a51dfa54f506e5fb354
Uladus93 <Loscros88@gmail.com>
2022-12-22 13:17:25 +0300

    Added restart and game over
    created button restart and upgraded gameover's logic.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _gameOver;
+    [SerializeField] private Button _restart;
     private static bool _end = false;
     public static bool End
     {
@@ -16,6 +21,16 @@ public class GameOver : MonoBehaviour
         if (Lifes.Life < 1)
         {
             End = true;
+            _gameOver.gameObject.SetActive(true);
+            _restart.gameObject.SetActive(true);
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Lifes.Life = 3;
+        Score.PlayerScore = 0;
+        End = false;
     }
 }
